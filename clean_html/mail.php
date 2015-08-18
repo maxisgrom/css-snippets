d6fd26d765418354e728929f8dd52b62f04a1b64
Max Grom <maxwallaby@yandex.ru>
2015-08-18 22:03:06 +0300

    Little fix

--- clean_html/mail.php
+++ clean_html/mail.php
@@ -1,7 +1,7 @@
 <?php
 
 $frm_name  = "Youname";
-$recepient = "youmail@google.com";
+$recepient = "youmail@fb.com";
 $sitename  = "Название Сайта";
 $subject   = "Новая заявка с сайта \"$sitename\"";
 
